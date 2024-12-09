c43feb85e30c5f9722e6ebeda26ad7ab7c1e9d90
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-12-09 17:11:21 +0600

    doctor commit

--- Doctors/admin.py
+++ Doctors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from.models import Weekly_Days,Designation, Doctor
 
 # Register your models here.
+admin.site.register(Weekly_Days)
+admin.site.register(Designation)
+admin.site.register(Doctor)
--- Doctors/models.py
+++ Doctors/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from Department.models import Departments
+from Branches.models import Branches
 
 # Create your models here.
+class Weekly_Days(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+       return self.name
+    
+class Designation (models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+       return self.name
+    
+class Doctor (models.Model):
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to="Doctors/images/")
+    designation = models.ForeignKey(Designation,on_delete=models.CASCADE)
+    specialities = models.ManyToManyField(Departments,related_name="doctors")
+    branch = models.ManyToManyField(Branches,related_name="doctors")
+    degree = models.TextField(max_length=10000)
+    practice_day = models.ManyToManyField(Weekly_Days,related_name="doctors")
+    
+    def __str__(self):
+        return self.name
